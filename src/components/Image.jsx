dd10390ddf5e7afa0b3ed5ebade65d8eedaa774f
aamirusman1 <aamirbinusman@gmail.com>
2025-01-24 18:47:03 +0530

    image optimization using imagekit.io

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { IKImage } from "imagekitio-react";
 
 const Image = ({ src, className, w, h, alt }) => {
   return (
     <>
-      <img
-        src={src}
+      <IKImage
+        urlEndpoint={import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT}
+        path={src}
+        className={className}
+        loading="lazy"
+        lqip={{ active: true, quality: 20 }}
         alt={alt}
         width={w}
         height={h}
-        className={className}
         transformation={[
           {
             width: w,
